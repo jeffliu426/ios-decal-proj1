7d8d8e122cac72f8745350ed314b6ff6c1fe9246
Jeffrey Liu <jliu23@berkeley.edu>
2016-03-01 23:55:41 -0800

    changed time limit variable to 24 hours

--- To-Do-List/To-Do-List/Task.swift
+++ To-Do-List/To-Do-List/Task.swift
@@ -16,7 +16,7 @@ struct Task {
     var timeCompleted: NSTimeInterval?
     var isDeletedBefore24Hours: Bool?
     var isDeletedAfter24Hours: Bool?
-    static var timeLimit = 86400
+    static var timeLimit = 86400.0
     
     init(name: String?, isCompleted: Bool?, timeCompleted: NSTimeInterval?, isDeletedBefore24Hours: Bool?, isDeletedAfter24Hours: Bool?) {
         self.name = name
